df69d7475fe21826b826c795c92dbdd77f25e39c
JamesMc <jamesjohnmcguire@gmail.com>
2021-10-04 17:55:12 +0900

    Added testing framework.

---
+++ Tests/UnitTests.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @author    James John McGuire <jamesjohnmcguire@gmail.com>
+ * @copyright 2021 James John McGuire
+ */
+declare(strict_types=1);
+
+namespace DigitalZenWorks\PhpImageOptimizer\UnitTests;
+
+require 'vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ */
+final class UnitTests extends TestCase
+{
+	protected function setUp() : void
+	{
+	}
+
+	public static function setUpBeforeClass() : void
+	{
+	}
+}
