a995186fc1a56acf1e7d15e32e1244bcb2e68b39
JamesMc <jamesjohnmcguire@gmail.com>
2021-10-05 23:13:48 +0900

    Build script improvements.

--- DevelopmentTools/Commands/build.cmd
+++ DevelopmentTools/Commands/build.cmd
@@ -5,4 +5,20 @@ ECHO PHP code styles
 CALL ..\vendor\bin\phpcs -sp --standard=ruleset.xml .
 
 CD ..
-CALL phpdocumentor --setting=graphs.enabled=true -d SourceCode -t Documentation
+CALL vendor\bin\phpunit -c Tests\phpunit.xml Tests\UnitTests.php %1 %2
+
+if "%1" == "release" GOTO release
+GOTO end
+
+:release
+ECHO Release
+CD SourceCode
+CALL VersionUpdate ImageOptimizer.php
+cd ..\Tests
+CALL VersionUpdate tests.php
+
+REM Currently, not working on windows - phpdocumentor bug
+REM CALL phpdocumentor --setting=graphs.enabled=true -d SourceCode -t Documentation
+
+:end
+CD ..
