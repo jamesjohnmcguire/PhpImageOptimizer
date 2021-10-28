4c8d5123235db6fcfb21f4b07ba71945256ea6b1
JamesMc <jamesjohnmcguire@gmail.com>
2021-10-28 17:11:58 +0900

    Documentation updates.

--- Documentation/js/searchIndex.js
+++ Documentation/js/searchIndex.js
@@ -3,28 +3,73 @@ Search.appendIndex(
                 {
             "fqsen": "\\DigitalZenWorks\\ImageOptimizer",
             "name": "ImageOptimizer",
-            "summary": "ImageOptimizer\u0020\u002D\u0020An\u0020Imagick\u0020extension\u0020to\u0020provide\u0020better\u0020\u0028higher\u0020quality,\u0020lower\u0020file\u0020size\u0029\u0020image\u0020resizes.",
+            "summary": "ImageOptimizer\u0020\u002D\u0020An\u0020Imagick\u0020extension\u0020to\u0020provide\u0020better\u0020\u0028higher\u0020quality,\nlower\u0020file\u0020size\u0029\u0020image\u0020resizes.",
             "url": "classes/DigitalZenWorks-ImageOptimizer.html"
+        },                {
+            "fqsen": "\\DigitalZenWorks\\ImageOptimizer\u003A\u003AalternateResize\u0028\u0029",
+            "name": "alternateResize",
+            "summary": "AlternateResize\u0020method.",
+            "url": "classes/DigitalZenWorks-ImageOptimizer.html#method_alternateResize"
+        },                {
+            "fqsen": "\\DigitalZenWorks\\ImageOptimizer\u003A\u003AisImageOptimEnabled\u0028\u0029",
+            "name": "isImageOptimEnabled",
+            "summary": "Checks\u0020if\u0020the\u0020imageoptim\u0020program\u0020is\u0020available\u0020on\u0020the\u0020\u0024PATH.",
+            "url": "classes/DigitalZenWorks-ImageOptimizer.html#method_isImageOptimEnabled"
+        },                {
+            "fqsen": "\\DigitalZenWorks\\ImageOptimizer\u003A\u003AisImageUnderscoreOptimEnabled\u0028\u0029",
+            "name": "isImageUnderscoreOptimEnabled",
+            "summary": "Checks\u0020if\u0020the\u0020image_optim\u0020program\u0020is\u0020available\u0020on\u0020the\u0020\u0024PATH.",
+            "url": "classes/DigitalZenWorks-ImageOptimizer.html#method_isImageUnderscoreOptimEnabled"
+        },                {
+            "fqsen": "\\DigitalZenWorks\\ImageOptimizer\u003A\u003AisPicOptEnabled\u0028\u0029",
+            "name": "isPicOptEnabled",
+            "summary": "Checks\u0020if\u0020the\u0020picopt\u0020program\u0020is\u0020available\u0020on\u0020the\u0020\u0024PATH.",
+            "url": "classes/DigitalZenWorks-ImageOptimizer.html#method_isPicOptEnabled"
+        },                {
+            "fqsen": "\\DigitalZenWorks\\ImageOptimizer\u003A\u003AisSvgoEnabled\u0028\u0029",
+            "name": "isSvgoEnabled",
+            "summary": "Checks\u0020if\u0020the\u0020svgo\u0020program\u0020is\u0020available\u0020on\u0020the\u0020\u0024PATH.",
+            "url": "classes/DigitalZenWorks-ImageOptimizer.html#method_isSvgoEnabled"
+        },                {
+            "fqsen": "\\DigitalZenWorks\\ImageOptimizer\u003A\u003AoptimalImage\u0028\u0029",
+            "name": "optimalImage",
+            "summary": "Changes\u0020the\u0020size\u0020of\u0020an\u0020image\u0020to\u0020the\u0020given\u0020dimensions\u0020and\nremoves\u0020any\u0020associated\u0020profiles.",
+            "url": "classes/DigitalZenWorks-ImageOptimizer.html#method_optimalImage"
         },                {
             "fqsen": "\\DigitalZenWorks\\ImageOptimizer\u003A\u003Aoptimize\u0028\u0029",
             "name": "optimize",
             "summary": "Optimizes\u0020the\u0020image\u0020without\u0020reducing\u0020quality.",
             "url": "classes/DigitalZenWorks-ImageOptimizer.html#method_optimize"
         },                {
-            "fqsen": "\\DigitalZenWorks\\ImageOptimizer\u003A\u003Aresize\u0028\u0029",
-            "name": "resize",
-            "summary": "Resize\u0020method.",
-            "url": "classes/DigitalZenWorks-ImageOptimizer.html#method_resize"
-        },                {
             "fqsen": "\\DigitalZenWorks\\ImageOptimizer\u003A\u003AsmartResize\u0028\u0029",
             "name": "smartResize",
             "summary": "Resizes\u0020the\u0020image\u0020using\u0020smart\u0020defaults\u0020for\u0020high\u0020quality\u0020and\nlow\u0020file\u0020size.",
             "url": "classes/DigitalZenWorks-ImageOptimizer.html#method_smartResize"
         },                {
-            "fqsen": "\\DigitalZenWorks\\ImageOptimizer\u003A\u003AoptimalImage\u0028\u0029",
-            "name": "optimalImage",
-            "summary": "Changes\u0020the\u0020size\u0020of\u0020an\u0020image\u0020to\u0020the\u0020given\u0020dimensions\u0020and\nremoves\u0020any\u0020associated\u0020profiles.",
-            "url": "classes/DigitalZenWorks-ImageOptimizer.html#method_optimalImage"
+            "fqsen": "\\DigitalZenWorks\\ImageOptimizer\u003A\u003AsmartResizeFile\u0028\u0029",
+            "name": "smartResizeFile",
+            "summary": "Easy\u0020image\u0020resize\u0020function.",
+            "url": "classes/DigitalZenWorks-ImageOptimizer.html#method_smartResizeFile"
+        },                {
+            "fqsen": "\\DigitalZenWorks\\ImageOptimizer\u003A\u003AsmartResizeImage\u0028\u0029",
+            "name": "smartResizeImage",
+            "summary": "Easy\u0020image\u0020resize\u0020function.",
+            "url": "classes/DigitalZenWorks-ImageOptimizer.html#method_smartResizeImage"
+        },                {
+            "fqsen": "\\DigitalZenWorks\\ImageOptimizer\u003A\u003AcalculateDestinationDimensions\u0028\u0029",
+            "name": "calculateDestinationDimensions",
+            "summary": "Calculates\u0020the\u0020destination\u0020dimensions.",
+            "url": "classes/DigitalZenWorks-ImageOptimizer.html#method_calculateDestinationDimensions"
+        },                {
+            "fqsen": "\\DigitalZenWorks\\ImageOptimizer\u003A\u003AisExternalProgramEnabled\u0028\u0029",
+            "name": "isExternalProgramEnabled",
+            "summary": "Checks\u0020if\u0020the\u0020external\u0020program\u0020is\u0020available\u0020on\u0020the\u0020\u0024PATH.",
+            "url": "classes/DigitalZenWorks-ImageOptimizer.html#method_isExternalProgramEnabled"
+        },                {
+            "fqsen": "\\DigitalZenWorks\\ImageOptimizer\u003A\u003AsvgoOptimize\u0028\u0029",
+            "name": "svgoOptimize",
+            "summary": "Optimize\u0020an\u0020image\u0020using\u0020the\u0020svgo\u0020program.",
+            "url": "classes/DigitalZenWorks-ImageOptimizer.html#method_svgoOptimize"
         },                {
             "fqsen": "\\",
             "name": "\\",
