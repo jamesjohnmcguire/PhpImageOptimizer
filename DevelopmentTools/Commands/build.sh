6023d41982dd2f1fbe9e4196de93807aa0b6ca47
JamesMc <jamesjohnmcguire@gmail.com>
2021-11-11 17:00:59 +0900

    Improves call to phpDocumentor

--- DevelopmentTools/Commands/build.sh
+++ DevelopmentTools/Commands/build.sh
@@ -11,5 +11,5 @@ if [[ $1 == "release" ]] ; then
 	echo "release Is set!"
 
 	rm -rf Documentation
-	php /mnt/c/Users/JamesMc/Data/External/Util/Bin/phpDocumentor.phar --setting="graphs.enabled=true" -d SourceCode -t Documentation
+	phpDocumentor.phar --setting="graphs.enabled=true" -d SourceCode -t Documentation
 fi
