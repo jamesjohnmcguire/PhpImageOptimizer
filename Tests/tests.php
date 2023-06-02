d5258e0df8ba72e916663c6cb878a93ceee96a0b
James John McGuire <jamesjohnmcguire@gmail.com>
2023-06-02 18:26:08 +0900

    Enhance Tests to Check Previous Results

--- Tests/tests.php
+++ Tests/tests.php
@@ -34,6 +34,85 @@ $widths =
 	320, 640, 1280
 ];
 
+// These are the saved results from previous runs.
+$respImgResults =
+[
+	'1A-1.jpg' =>
+	[
+		'320' =>
+		[
+			'size' => 17804,
+			'width' => 320,
+			'height' => 280
+		],
+		'640' =>
+		[
+			'size' => 51670,
+			'width' => 640,
+			'height' => 560
+		],
+		'1280' =>
+		[
+			'size' => 159779,
+			'width' => 1280,
+			'height' => 1120
+		]
+	],
+	'3C-2.png' =>
+	[
+		'320' =>
+		[
+			'size' => 27486,
+			'width' => 320,
+			'height' => 259
+		],
+		'640' =>
+		[
+			'size' => 74711,
+			'width' => 640,
+			'height' => 517
+		],
+		'1280' =>
+		[
+			'size' => 177569,
+			'width' => 1280,
+			'height' => 1035
+		]
+	],
+	'TesterImage6.jpg' =>
+	[
+		'320' =>
+		[
+			'size' => 16929,
+			'width' => 320,
+			'height' => 194
+		],
+		'640' =>
+		[
+			'size' => 54047,
+			'width' => 640,
+			'height' => 388
+		],
+		'1280' =>
+		[
+			'size' => 181505,
+			'width' => 1280,
+			'height' => 776
+		]
+	]
+];
+
+// Setup output directories.
+if (!file_exists($path_raster_o))
+{
+    mkdir($path_raster_o, 0777, true);
+}
+
+if (!file_exists($path_svg_o))
+{
+    mkdir($path_svg_o, 0777, true);
+}
+
 // resize raster inputs
 $directoryHandle = opendir($path_raster_i);
 
@@ -41,6 +120,8 @@ if ($directoryHandle !== false)
 {
 	while (($file = readdir($directoryHandle)) !== false)
 	 {
+		echo "Processing: $file\r\n";
+
 		$base = pathinfo($file, PATHINFO_BASENAME);
 		$extension = strtolower(pathinfo($file, PATHINFO_EXTENSION));
 
@@ -48,13 +129,44 @@ if ($directoryHandle !== false)
 		{
 			foreach ($widths as $width)
 			{
-				echo 'Resizing ' . $file . ' to ' . $width . '…';
+				echo 'Resizing to ' . $width . "…\r\n";
 				$image = new ImageOptimizer($path_raster_i . '/' . $file);
 				$image->smartResize($width, 0, true);
 				$destination = $path_raster_o . '/' . $base . '-w' . $width .
 					'.' . $extension;
 				$image->writeImage($destination);
-				echo "OK\n";
+				
+				echo "Comparing to previous results\r\n";
+				$baseName = basename($destination);
+				$baseName = str_replace(".$extension-w$width", '', $baseName);
+
+				$exists = array_key_exists($baseName,$respImgResults);
+
+				if ($exists === false)
+				{
+					echo "Warning: Results key missing for $baseName!\r\n";
+				}
+				else
+				{
+					$size = filesize($destination);
+
+					if ($size !== $respImgResults[$baseName][$width]['size'])
+					{
+						echo "File size different from original result!\r\n";
+					}
+
+					list($fileWidth, $height) = getimagesize($destination);
+
+					if ($fileWidth !== $respImgResults[$baseName][$width]['width'])
+					{
+						echo "Image width different from original result!\r\n";
+					}
+
+					if ($height !== $respImgResults[$baseName][$width]['height'])
+					{
+						echo "Image height different from original result!\r\n";
+					}
+				}
 			}
 		}
 	}
